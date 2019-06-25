b06ab8688c0be78ec7e567b0d69d16ee9e0ac46b
jpbetz <jpbetz@debian>
2019-06-25 01:25:47 -0700

    Fix powerline colors

--- config/config-modeline.el
+++ config/config-modeline.el
@@ -14,7 +14,14 @@
   ;; https://github.com/arcticicestudio/nord-emacs/blob/b5c1dc762fe3acaa88a0ce9640085d45d0109c43/nord-theme.el#L81
   (set-face-attribute 'spaceline-highlight-face nil :background (if (display-graphic-p) "#5E81AC" "brightblue")) ;; nord9
   (set-face-attribute 'spaceline-highlight-face nil :foreground (if (display-graphic-p) "#3B4252" "black")) ;; nord1
-  (set-face-attribute `mode-line nil :background (if (display-graphic-p) "#4C566A" "brightblack")) ;; nord3
+  (set-face-attribute `mode-line nil :background (if (display-graphic-p) "#4C566A" "brightblack")) ;; nord3, if terminal "brightblack" is too bright, use "black"
+  (set-face-attribute `mode-line-highlight nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
+  (set-face-attribute `powerline-active1 nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
+  (set-face-attribute `powerline-active2 nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
+
+  (set-face-attribute `mode-line-inactive nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
+  (set-face-attribute `powerline-inactive1 nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
+  (set-face-attribute `powerline-inactive2 nil :background (if (display-graphic-p) "#3B4252" "black")) ;; nord1
 
   ;; Hide buffer size. Not important enough to keep on the screen all the time.
   (spaceline-toggle-buffer-size-off)
